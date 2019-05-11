35d5b874f891b798703aafdbbdd2879095735b66
Zone <zonetw.web@gmail.com>
2019-05-11 10:59:28 +0800

    add App.vue,  show another way to show content on screen with render function

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Hi there !</div>
+</template>
+
+<script>
+export default {
+  // el > no need to add el,
+  name: "App" // add name to make debugging more easier
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import Vue from "vue"; // there is no vue.js in source folder, webpack will find it in node_modules folder
+import App from "./App";
+
 new Vue({
+    // Another way to show content on screen
+    // render: function (createElement) {
+    //     return createElement(App);
+    // }
+    render: h => h(App),
 });
