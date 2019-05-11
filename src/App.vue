bb5b6ce1102227b11047065f628d529f2087dba9
Zone <zonetw.web@gmail.com>
2019-05-11 15:22:57 +0800

    request youtube data api

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <SearchBar v-on:termChange="onTermChange"></SearchBar>
+  <div>
+    <SearchBar @termChange="onTermChange"></SearchBar>
+  </div>
 </template>
 
 <script>
 import SearchBar from "./components/SearchBar";
+import axios from "axios";
+
+const API_KEY = "AIzaSyBGqYeHI1j2-XRT0o5gAguVd12GVwgJPTI";
+
 export default {
   // el > no need to add el,
   name: "App", // add name to make debugging more easier
@@ -13,7 +19,17 @@ export default {
   },
   methods: {
     onTermChange: function(value) {
-      window.console.log("[App] " + value);
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: value
+          }
+        })
+        .then(response => {
+        });
     }
   }
 };
