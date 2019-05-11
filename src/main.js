007ff3e4ea48fd5e0df62a0c15ddbc84d7f8ba66
Zone <zonetw.web@gmail.com>
2019-05-11 11:00:25 +0800

    mount App component to DOM: Two methods, same result

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue"; // there is no vue.js in source folder, webpack will find
 import App from "./App";
 
 new Vue({
+    el: "#app",
     // Another way to show content on screen
     // render: function (createElement) {
     //     return createElement(App);
     // }
     render: h => h(App),
 });
+// .$mount("#app"); // same funcitionality as el setting
