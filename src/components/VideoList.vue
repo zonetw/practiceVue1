bf26ecef91c8f8d708542e94c34ed3a2a73b936b
Zone <zonetw.web@gmail.com>
2019-05-11 15:55:57 +0800

    VideoList get updated videos from props: 2 methods

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
     <ul>VideoList</ul>
+    {{videos.length}}
   </div>
 </template>
+
 <script>
 import VideoListItem from "./VideoListItem";
 
 export default {
   name: "VideoList",
+  // two way to setup props
+  // 1. an object with props name and type > TODO: What kind of type can we use in Vue ?
+  //   props: {
+  //       videos: Array
+  //   },
+  // 2. setup props in array
+  props: ["videos"]
 };
 </script>
