804af5eecfd17e7bc1115529e814ab283dbafa9f
Zone <zonetw.web@gmail.com>
2019-05-11 11:09:08 +0800

    add SearchBar.vue: one input element with input event handler

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>Hi there !</div>
+  <SearchBar></SearchBar>
 </template>
 
 <script>
+import SearchBar from "./components/SearchBar";
 export default {
   // el > no need to add el,
-  name: "App" // add name to make debugging more easier
+  name: "App", // add name to make debugging more easier
+  // use components object to setup the all components will be used inside the template
+  components: {
+    SearchBar
+  }
 };
 </script>
 
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input v-on:input="onTextInput">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onTextInput: function(e) {
+      window.console.log(e.target.value);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
