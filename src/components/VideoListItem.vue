199b7a054bb9b6ea27d3da9352a29c2808baf5b0
Zone <zonetw.web@gmail.com>
2019-05-12 21:50:41 +0800

    shorten thumbnail with computed function

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,13 +1,18 @@
 <template>
   <li class="list-group-item">
-    <img :src="videoInfo.snippet.thumbnails.default.url">
     {{videoInfo.snippet.title}}
+    <img :src="thumbnailUrl">
   </li>
 </template>
 
 <script>
 export default {
   name: "VideoListItem",
-  props: ["videoInfo"]
+  props: ["videoInfo"],
+  computed: {
+    thumbnailUrl: function() {
+      return this.videoInfo.snippet.thumbnails.default.url;
+    }
+  }
 };
 </script>
