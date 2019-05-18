041ad133326076a5f7da057e456c4cca14978823
Zone <zonetw.web@gmail.com>
2019-05-18 16:57:46 +0800

    use imgur api to fetchImage

--- image/src/api/imgur.js
+++ image/src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from "qs";
+import axios from "axios";
 
 const CLIENT_ID = "c7ba4200a3e8056";
 const ROOT_URL = "https://api.imgur.com";
@@ -12,5 +13,12 @@ export default {
 
         const url = `${ROOT_URL}/oauth2/authorize?${qs.stringify(queryString)}`;
         window.location = url;
+    },
+    fetchImage(token) {
+        return axios.get(`${ROOT_URL}/3/account/me/images`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
     }
 }
