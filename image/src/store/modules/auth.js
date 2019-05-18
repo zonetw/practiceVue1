8be94f2cbedd8f798813bcfa6ef955bbb4978358
Zone <zonetw.web@gmail.com>
2019-05-18 11:07:13 +0800

    add mutation method: finalizeLogin in Vuex store

--- image/src/store/modules/auth.js
+++ image/src/store/modules/auth.js
@@ -1,4 +1,5 @@
 import api from "../../api/imgur";
+import qs from "qs";
 
 const state = {
     token: null,
@@ -19,6 +20,10 @@ const mutations = {
 const actions = {
     login() {
         api.login();
+    },
+    finalizeLogin({ commit }, hash) {
+        const cbResult = qs.parse(hash.replace("#", ""));
+        commit("setToken", cbResult.access_token);
     },
     logout({ commit }) {
         commit("setToken", null);
