c38307ad0d6f16d3325cbb8e26834b70fcbcfb88
Zone <zonetw.web@gmail.com>
2019-05-18 09:50:08 +0800

    add AppHeader component

--- image/src/App.vue
+++ image/src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
+    <AppHeader></AppHeader>
   </div>
 </template>
 <script>
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
   name: "app",
+  components: {
+    AppHeader,
+  }
 };
 </script>
 
