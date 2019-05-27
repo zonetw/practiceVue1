fac513f49d58d4e98ad2bf960241fcff54340e0a
Zone <zonetw.web@gmail.com>
2019-05-28 00:12:03 +0800

    style App.vue

--- imageUploader/src/App.vue
+++ imageUploader/src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <div id="app">
-    <AppHeader></AppHeader>
-    <!-- router-view will be replaced by another component by VueRouter's config -->
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <AppHeader></AppHeader>
+        <div class="ui container">
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
 <script>
-import AppHeader from "./components/AppHeader.vue";
+    import AppHeader from "./components/AppHeader.vue";
 
-export default {
-  name: "app",
-  components: {
-    AppHeader
-  }
-};
+    export default {
+        name: "app",
+        components: {
+            AppHeader
+        }
+    };
 </script>
 
 <style>
